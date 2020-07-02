6b7ae8184feb6767cb7efaaf3cc7438cf0196aeb
prashant1529 <64468525+prashant1529@users.noreply.github.com>
2020-07-02 11:44:40 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,13 +1,13 @@
-import SMTP
+import smtplib
 sender_email = "awesomeprince1530@gmail.com"
 receiver_email = "awesomeprince1530@gmail.com"
 password = "eL0nMu$K15"
 message = "Your code is not correct"
-server = smtplib.SMTP('smtp.gmail.com',465)
+server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login(sender_email,password)
 print('login successfully')
 
-server.sendmail(sender_email,receiver_email , meassge)
-print("Email has been sent to " ,receiver_email)
+server.sendmail(sender_email,receiver_email,meassge)
+print("Email has been sent to ",receiver_email)
 server.quit()
